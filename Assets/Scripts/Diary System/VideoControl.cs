a93edf5dda976c7ee56b87f2b3580ea658e177ce
Timo <me@timoheijne.nl>
2018-04-05 14:10:37 +0200

    Added video player and some test videos

---
+++ Assets/Scripts/Diary System/VideoControl.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+[RequireComponent(typeof(VideoPlayer))]
+public class VideoControl : MonoBehaviour {
+
+	private VideoPlayer _videoPlayer;
+	
+	// Use this for initialization
+	void Start () {
+		_videoPlayer = GetComponent<VideoPlayer>();
+		
+		_videoPlayer.loopPointReached += VideoPlayerOnLoopPointReached;
+	}
+
+	private void VideoPlayerOnLoopPointReached(VideoPlayer source) {
+		// Means the video has finished we can hide the plane now
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
