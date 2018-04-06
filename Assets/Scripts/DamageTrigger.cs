4c4304482e9f3045b73db31a4c86411c0bc27220
Koen Sparreboom <koensparreboom@gmail.com>
2018-04-06 17:25:28 +0200

    Added DamageTrigger.cs

---
+++ Assets/Scripts/DamageTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using UnityEngine;
+
+public class DamageTrigger : MonoBehaviour {
+    [SerializeField]
+    private float _damage = 5;
+
+    private bool _enabled;
+
+    public bool Enabled {
+        get { return _enabled; }
+        set { _enabled = value; }
+    }
+
+    [SerializeField]
+    private string[] _excludedTags;
+
+    private void OnTriggerEnter(Collider other) {
+        if (_enabled && !_excludedTags.Contains(other.tag)) {
+            other.GetComponent<Health>()?.TakeDamage(_damage);
+            print(other.GetComponent<Health>()?.CurHealth);
+        }
+    }
+}
