857869c0f59e3858a1b8cd38ed653620b9555a99
Timo Heijne <me@timoheijne.nl>
2018-04-12 21:56:35 +0200

    ALL THE ASSETS ARE FUCKING RESERIALIZED... and Crafting Object + custom editor menu... also basic UI for the inventory

---
+++ Assets/Scripts/Inventory System/UI/UIDataSeeder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIDataSeeder : MonoBehaviour {
+
+    [SerializeField] private Text _itemName;
+    [SerializeField] private Text _itemDescription;
+    [SerializeField] private Image _itemIcon;
+    [SerializeField] private Text _itemAmount;
+    [SerializeField] private Button _craftButton;
+    [SerializeField] private Button _equipButton;
+    [SerializeField] private Button _dropButton;
+    
+    public void SetItem(ItemObject itemObject) {
+        _itemName.text = itemObject.ItemName;
+        _itemDescription.text = itemObject.ItemDescription;
+        _itemIcon.sprite = itemObject.ItemIcon;
+        
+    }
+
+    public void SetCraftable() {
+        throw new NotImplementedException();
+    }
+}
