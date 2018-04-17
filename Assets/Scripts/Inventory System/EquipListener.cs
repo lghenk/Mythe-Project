4a6a654a279c49c718d8f340ef228121c274cee5
Timo Heijne <me@timoheijne.nl>
2018-04-17 17:23:02 +0200

    Can now pickup items and Equip weapons

---
+++ Assets/Scripts/Inventory System/EquipListener.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class EquipListener : MonoBehaviour {
+	private void Start() {
+		Inventory.Instance.onItemEquip += OnItemEquip;
+	}
+
+	protected virtual void OnItemEquip(ItemObject itemObject) {
+		throw new NotImplementedException();
+	}
+}
