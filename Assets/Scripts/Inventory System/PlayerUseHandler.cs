8f94d51a5ac0a748d211ef9e9e1f8bc6cb11704c
Timo Heijne <me@timoheijne.nl>
2018-04-17 23:10:35 +0200

    Added Action to DamageTrigger, Build Tested the game (works)

--- Assets/Scripts/Inventory System/PlayerUseHandler.cs
+++ Assets/Scripts/Inventory System/PlayerUseHandler.cs
@@ -12,7 +12,6 @@ using UnityEngine;
 public class PlayerUseHandler : EquipListener {
     protected override void OnItemEquip(ItemObject itemObject) { 
         print("Equip... 2");
-
         if (itemObject.Type == ItemObject.ItemType.Usable) {
             var assembly = Assembly.GetExecutingAssembly();
 
@@ -22,13 +21,15 @@ public class PlayerUseHandler : EquipListener {
             if (type == null) {
                 throw new Exception($"Use Behaviour Class \"{itemObject.UseBehaviour}\" Not Found!");
             }
-
+            Inventory.Instance.RemoveItem(itemObject);
+            
             // Unity will complain about this, however this is the best and loosly coupled method i could find that works
             // You CAN NOT add monobehaviour or any other script types to a ScriptableObject. otherwise that would've been the solution
             // so this'll do
             UseBehaviour ub = (UseBehaviour)Activator.CreateInstance(type);
             ub.Execute(this.gameObject);
-            Inventory.Instance.RemoveItem(itemObject);
+            Destroy(ub); // Don't forget to destroy that object again... Prevents double adding scripts
+            
         }
     }
 }
