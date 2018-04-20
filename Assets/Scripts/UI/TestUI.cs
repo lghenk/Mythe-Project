bffe0fb05517cfd95f5f1515bb4300ad526c8274
Koen Sparreboom <koensparreboom@gmail.com>
2018-04-20 17:08:24 +0200

    Wat UI dingen

---
+++ Assets/Scripts/UI/TestUI.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class TestUI : MonoBehaviour {
+	[SerializeField]
+	private PanelUI _panel;
+	
+	private void Update() {
+		if (Input.GetKeyDown(KeyCode.A)) {
+			_panel.Visible = !_panel.Visible;
+		}
+	}
+}
