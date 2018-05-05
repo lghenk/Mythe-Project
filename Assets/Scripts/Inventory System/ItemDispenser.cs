f5b7d38a0a6f1e31c557d4915bf9d998d6acd80e
Timo Heijne <me@timoheijne.nl>
2018-05-05 17:44:31 +0200

    Added item dispensers, Implemented Null checking in inventory

---
+++ Assets/Scripts/Inventory System/ItemDispenser.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary author="Timo Heijne">
+/// This will be attached to every object that can dispense an item to the player
+/// </summary>
+public class ItemDispenser : MonoBehaviour {
+    [SerializeField] private ItemObject _itemObject;
+    [SerializeField] private int _numItems = 2;
+
+    public ItemObject TakeItem() {
+        if (_numItems <= 0) return null;
+        
+        _numItems--;
+        return _itemObject;
+    }
+}
---
+++ Assets/Scripts/Inventory System/PlayerDispenserHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary author="Timo Heijne">
+/// This class handles the dispenser on a hit of a weapon
+/// </summary>
+public class PlayerDispenserHandler : MonoBehaviour {
+
+	private MeleeCombat _meleeCombat;
+	
+	// Use this for initialization
+	void Start () {
+		_meleeCombat = GetComponent<MeleeCombat>();
+		_meleeCombat.onHit += OnHit;
+	}
+
+	private void OnHit(Transform transform) {
+		ItemDispenser id = transform.GetComponent<ItemDispenser>();
+		if (id != null) {
+			ItemObject io = id.TakeItem();			
+			Inventory.Instance.AddItem(io);
+		}
+	}
+}
