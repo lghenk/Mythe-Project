2ce57816f0688fa5b181709a2bb0d00eb8f015bc
Koen Sparreboom <koensparreboom@gmail.com>
2018-05-07 05:59:21 +0200

    ui en dingen

---
+++ Assets/Scripts/UI/BasicButton.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BasicButton : MonoBehaviour {
+    public void LoadLevel(int levelIndex) {
+        SceneManager.LoadScene(levelIndex);
+    }
+
+    public void LoadLevel(string levelName) {
+        SceneManager.LoadScene(levelName);
+    }
+
+    public void QuitApplication() {
+        Application.Quit();
+    }
+}
