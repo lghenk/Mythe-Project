6a32ac848466caaab18caccd48a4b94fc6a97bd1
Timo Heijne <me@timoheijne.nl>
2018-05-08 22:20:36 +0200

    Added death screen from UI

---
+++ Assets/Scripts/PlayerDeathEvent.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDeathEvent : MonoBehaviour {
+
+	private Health _health;
+
+	[SerializeField] private GameObject _deathScreen;
+	
+	// Use this for initialization
+	void Start () {
+		_health = GetComponent<Health>();
+		_health.onDeath += OnDeath;
+	}
+
+	private void OnDeath(Health health) {
+		_deathScreen.SetActive(true);
+	}
+}
