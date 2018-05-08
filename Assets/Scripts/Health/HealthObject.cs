385b01e6781d40ccead436d9d279840604550e32
Timo <me@timoheijne.nl>
2018-05-08 13:21:06 +0200

    health Trees

---
+++ Assets/Scripts/Health/HealthObject.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthObject : MonoBehaviour {
+
+    [SerializeField, Tooltip("The amount of health this object can spit out")] 
+    private float _currentHealth = 100;
+    public float CurrentHealth => _currentHealth;
+
+    public float TakeHealth(float amount = -1) {
+        if (amount == -1) {
+            _currentHealth = 0;
+            return _currentHealth;
+        } else {
+            if (_currentHealth - amount >= 0) {
+                _currentHealth -= amount;
+                return amount;
+            } else {
+                _currentHealth = 0;
+                return _currentHealth;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Health/HealthObjectHandler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthObjectHandler : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.E)) {
+			RaycastHit hit;
+			if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, 10)) {
+				HealthObject healthobj = hit.collider.GetComponent<HealthObject>();
+				if (!healthobj) return;
+					
+				Health health = GetComponent<Health>();
+				if (!health) return;
+
+				float required = health.StartingHealth - health.CurHealth;
+				float amount = healthobj.TakeHealth(required);
+				health.AddHealth(amount);
+			}
+		}
+	}
+}
