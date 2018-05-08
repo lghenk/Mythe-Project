4c6fdba27b888e3a491cc34650217d3f60f20306
Koen Sparreboom <koen@Koens-MacBook-Pro.local>
2018-05-08 12:37:23 +0200

    Added prefabs

--- Assets/Scripts/Extensions/ColorExtension.cs
+++ Assets/Scripts/Extensions/ColorExtension.cs
@@ -2,7 +2,7 @@
 
 public static class ColorExtension {
 
-	public static Color GetNewAlpha(this Color c, float value) {
+	public static Color ReturnNewColorThatsTheSameButWithDifferentAlphaDespacito(this Color c, float value) {
 		var newC = new Color(c.r, c.g, c.b, value);
 
 		return newC;
